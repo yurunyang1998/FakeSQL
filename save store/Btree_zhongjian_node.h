3ebffc91ffca8e31e9d0e3917f0f92862a6cab6d
windski <1272162048@qq.com>
2018-02-19 21:16:06 +0800

    Update Btree_zhongjian_node.h
    fixed potential errors.

--- save store/Btree_zhongjian_node.h
+++ save store/Btree_zhongjian_node.h
@@ -1,51 +1,38 @@
 //
 // Created by yurunyang on 18-2-16.
 //
-#include "Btree_leaf_node.h"
-
 #ifndef FAKESQL_BTREE_ZHONGJIAN_NODE_H
 #define FAKESQL_BTREE_ZHONGJIAN_NODE_H
+#include "Btree_leaf_node.h"
 
 namespace utils {
 
-template <class key_type >
+template <class key_type>
 class zhongjian_Node{
 public:
     zhongjian_Node()
     {
-
-    };
+    }
 
     void set_parent_node(zhongjian_Node<key_type> *parent_copy)
     {
-        try{
-            this->parent=parent_copy;
-            cout<<"set parent_node succeed"<<endl;
-        }catch (...)
-        {
-
-        }
+        this->parent=parent_copy;
+        cout<<"set parent_node succeed"<<endl;
     }
 
     int spile_node();   //分裂自己
 
-
 private:
-
     zhongjian_Node<key_type>  *parent;   //父节点
-
-
     key_type key0;
     key_type key1;
     key_type key2;
     key_type key3;
-
     Btree_leaf_Node<key_type>   *index0;       //key <0
     Btree_leaf_Node<key_type>   *index0_1;         //0<=key<1
     Btree_leaf_Node<key_type>   *index1_2;         //1<= key <2
     Btree_leaf_Node<key_type>   *index2_3;         //2<=key <3
     Btree_leaf_Node<key_type>   *index3_;          //3 <= key
-
 };
 
 } // end of utils
