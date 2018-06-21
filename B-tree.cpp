325966025ba92b7004bfebfb80cc9018cdc1407c
yurunyang1998 <674860268@qq.com>
2018-06-21 18:10:11 +0800

    今天就这样吧,节点之间的插入都搞定了耶

--- B-tree.cpp
+++ B-tree.cpp
@@ -11,13 +11,180 @@ namespace utlis {
 
 
 
+    template <class key_type>
+    class key_value_pair_for_middle_node;
+
+    template <class key_type,class value_type>
+    class leaf_node;
+
+    template <class key_type>
+    class key_value_pair_for_middle_node_which_next_node_is_leaf_node;
+
+    template <class key_type>
+    class middle_node;
+
+
+
+
+
+
+    template <class key_type>
+    class key_value_pair_for_middle_node
+    {
+        typedef middle_node<key_type>  middle_node_t;
+    public:
+
+        key_type key;
+        middle_node_t * value  ;
+
+        //leaf_node<key_type,int>   *leaf_node1;
+
+    public:
+        key_value_pair_for_middle_node(key_type key=nullptr, middle_node_t * value= nullptr):
+                key(key),value(value)
+        {
+//            this->key=key;
+//            this->value=value;
+        }
+
+
+
+
+        key_type getkey(void) { return key ; }
+        middle_node_t * getvalue(void) { return  value; }
+        void set_key(key_type  set_key)
+        {
+            key = set_key;
+            //return 0;
+        }
+
+        void set_value(middle_node_t * set_value )
+        {
+            value = set_value;
+        }
+    };
+
+
+
+
+
+
+    template <class key_type>
+    class key_value_pair_for_middle_node_which_next_node_is_leaf_node
+    {
+        typedef middle_node<key_type>  middle_node_t;
+    public:
+
+        key_type key;
+        //middle_node_t * value  ;
+
+        leaf_node<key_type,int>   * value;
+
+    public:
+
+
+        key_value_pair_for_middle_node_which_next_node_is_leaf_node
+                (key_type key=nullptr, leaf_node<key_type,int> * value= nullptr):
+                key(key),value(value)
+        {
+
+        }
+
+
+        key_type getkey(void) { return key ; }
+        leaf_node<key_type,int> * getvalue(void) { return  value; }
+        void set_key(key_type  set_key)
+        {
+            key = set_key;
+            //return 0;
+        }
+
+        void set_value(leaf_node<key_type,int>  * set_value )
+        {
+            value = set_value;
+        }
+    };
+
+
+
+    template <class key_type>
+    class middle_node
+    {
+
+    private:
+        typedef middle_node<key_type> middle_node_t;
+
+        //middle_node<key_type> * parent_node = new middle_node<key_type>;
+        //middle_node_t * parent_node;
+        int flag =-1;
+        vector< key_value_pair_for_middle_node<key_type> >   key_value_pair_for_middle_node_t ;
+        vector< key_value_pair_for_middle_node_which_next_node_is_leaf_node<key_type> >
+                key_value_pair_for_middle_node_which_next_node_is_leaf_node_t ;
+
+        //vector< key_value_pair_for_middle_node<key_type> > ;
+    public:
+        middle_node()//middle_node_t  * _parent_node = nullptr)
+        //: parent_node(_parent_node)
+        {
+
+        }
+        void insert(key_type key , middle_node_t * value)
+        {
+            if(flag==1)
+                return; //子节点不可同时存在既有叶子节点又有中间节点的情况
+            key_value_pair_for_middle_node_t.push_back( key_value_pair_for_middle_node <key_type>(key,value));
+            flag = 0;
+            sort();
+        }
+
+        void insert(key_type key , leaf_node<key_type,int> * value)
+        {
+            if(flag==0)
+                return; //子节点不可同时存在既有叶子节点又有中间节点的情况
+            key_value_pair_for_middle_node_which_next_node_is_leaf_node_t.push_back
+                    ( key_value_pair_for_middle_node_which_next_node_is_leaf_node<key_type>(key,value));
+            flag =1;
+            sort();
+        }
+
+
+
+        void sort()
+        {
+            if(flag ==0)
+                std::sort(key_value_pair_for_middle_node_t.begin()
+                        ,key_value_pair_for_middle_node_t.end());
+            if(flag ==1)
+                std::sort(key_value_pair_for_middle_node_which_next_node_is_leaf_node_t.begin(),
+                          key_value_pair_for_middle_node_which_next_node_is_leaf_node_t.end());
+
+
+        }
+
+        friend bool operator< ( key_value_pair_for_middle_node<key_type> &s1, key_value_pair_for_middle_node<key_type> &s2);
+
+                             //下一个节点是叶子节点的中间节点的用于sort的友元函数
+        friend bool operator< (key_value_pair_for_middle_node_which_next_node_is_leaf_node<key_type> &s1,
+                        key_value_pair_for_middle_node_which_next_node_is_leaf_node<key_type> &s2);
+
+
+
+
+    };
+
+
+
+
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    //下面是叶子节点类
 
 
 
     template <class key_type,class value_type>
     class key_value_pair
     {
-    public:
+    private:
 
         key_type key;
         value_type value;
@@ -39,94 +206,6 @@ namespace utlis {
             value = set_value;
         }
     };
-
-
-
-    template <class key_type>
-    class key_value_pair_for_middle_node;
-
-    template <class key_type,class value_type>
-    class leaf_node;
-
-
-    template <class key_type>
-    class middle_node
-    {
-
-    private:
-        typedef middle_node<key_type> middle_node_t;
-
-        //middle_node<key_type> * parent_node = new middle_node<key_type>;
-        //middle_node_t * parent_node;
-        vector< key_value_pair_for_middle_node<key_type> >   key_value_pair_for_middle_node_t ;
-
-
-               //vector< key_value_pair_for_middle_node<key_type> > ;
-    public:
-        middle_node()//middle_node_t  * _parent_node = nullptr)
-        //: parent_node(_parent_node)
-        {
-
-        }
-        void insert(key_type key , middle_node_t * value)
-        {
-            key_value_pair_for_middle_node_t.push_back( key_value_pair_for_middle_node <key_type>(key,value));
-        }
-
-        void insert(key_type key , leaf_node<key_type,int> * value)
-        {
-            key_value_pair_for_middle_node_t.push_back( key_value_pair_for_middle_node <key_type>(key,value));
-        }
-    };
-
-
-
-
-
-
-
-
-    template <class key_type>
-    class key_value_pair_for_middle_node
-    {
-        typedef middle_node<key_type>  middle_node_t;
-    private:
-
-        key_type key;
-        middle_node_t * value  ;
-
-        leaf_node<key_type,int>   *leaf_node1;
-
-    public:
-        key_value_pair_for_middle_node(key_type key=nullptr, middle_node_t * value= nullptr):
-                key(key),value(value)
-        {
-//            this->key=key;
-//            this->value=value;
-        }
-
-
-        key_value_pair_for_middle_node(key_type key=nullptr, leaf_node<key_type,int> * value= nullptr):
-                key(key),leaf_node1(value)
-        {
-
-        }
-
-
-        key_type getkey(void) { return key ; }
-        middle_node_t * getvalue(void) { return  value; }
-        void set_key(key_type  set_key)
-        {
-            key = set_key;
-            //return 0;
-        }
-
-        void set_value(middle_node_t * set_value )
-        {
-            value = set_value;
-        }
-    };
-
 
 
 
@@ -188,7 +267,6 @@ namespace utlis {
             return 0;
         }
 
-        //friend  bool compare(vector< key_value_pair<key_type,value_type> > leaf_node1,vector< key_value_pair<key_type,value_type> >leaf_node2);
 
         void _sort()
         {
@@ -214,6 +292,18 @@ namespace utlis {
     }
 
 
+    template <typename key_type>                        //中间节点内的用于sort的友元函数
+    bool operator< ( key_value_pair_for_middle_node<key_type> &s1, key_value_pair_for_middle_node<key_type> &s2)
+    {
+        return s1.key<s2.key;
+    }
+
+    template <typename  key_type>                       //下一个节点是叶子节点的中间节点的用于sort的友元函数
+    bool operator< (key_value_pair_for_middle_node_which_next_node_is_leaf_node<key_type> &s1,
+                    key_value_pair_for_middle_node_which_next_node_is_leaf_node<key_type> &s2)
+    {
+        return s1.key<s2.key;
+    }
 
 
 
--- main.cpp
+++ main.cpp
@@ -11,11 +11,29 @@ int main() {
     //middle_node<int> *c =  new middle_node<int>;
     //middle_node<int> *d =  new middle_node<int>;
 //
-    leaf_node<int,int> * b = new leaf_node<int,int>;
-    //a.insert(1,c);
-    leaf_node<int,int> * c = new leaf_node<int,int>;
+    middle_node<int> * b = new middle_node<int>;
+    middle_node<int> * c = new middle_node<int>;
+    middle_node<int> * d = new middle_node<int>;
+    middle_node<int> * e = new middle_node<int>;
+
+    leaf_node<int,int> * f = new leaf_node<int,int>;
+
+    leaf_node<int,int> * g = new leaf_node<int,int>;
+
+    leaf_node<int,int> * h = new leaf_node<int,int>;
+
+    leaf_node<int,int> * i = new leaf_node<int,int>;
+
+
+
     a.insert(2,b);
     a.insert(3,c);
+    a.insert(1,d);
+    a.insert(32,e);
+    e->insert(2,f);
+    e->insert(22,g);
+    e->insert(12,h);
+    b->insert(13,i);
     int s;
     return 0;
 
